8227d288b34e341d0a93c76e17b3ec3cf24dc88e
Andy Moon <mooner@andy-moon.com>
2018-05-02 21:04:20 -0500

    adding the Math Module assignment

---
+++ andy_moon/NODE/math_module/app.js
@@ -0,0 +1,6 @@
+var mathlib = require('./mathlib')();     //notice the extra invocation parentheses!
+mathlib.greet();
+mathlib.add(2,3);
+mathlib.multiply(3,5);
+mathlib.square(5);
+mathlib.random(1,10);
